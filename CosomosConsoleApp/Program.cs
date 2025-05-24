c9f2e0f8c6e603baeaa4f6a9c47912e1b829c7da
Hemanth <trhemanth2@gmail.com>
2025-05-24 13:40:07 +0530

    console scaffold

--- CosomosConsoleApp/Program.cs
+++ CosomosConsoleApp/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace CosmosConsoleApp;
+
+public class Program
+{
+	public static void Main(string[] args)
+	{
+		Console.WriteLine("Hello world");
+	}
+}
